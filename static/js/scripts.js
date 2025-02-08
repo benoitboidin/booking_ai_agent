c3b49df79e058a0a30f07af47ac2182792d0a17c
Boidin Benoît <benoit.bboidin@gmail.com>
2025-02-08 12:01:52 +0100

    Replace continuous recognition with restart to comply with Android devices

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,31 +1,10 @@
 let listening = false;
 let recognition = null; // Store recognition instance
 
-function speakText(text) {
-    if ('speechSynthesis' in window) {
-      const utterance = new SpeechSynthesisUtterance(text);
-      utterance.lang = 'fr-FR'; // Set language to French
-      utterance.rate = 1.1; // Adjust speed (1 is normal)
-      utterance.pitch = 0.9; // Adjust pitch (1 is normal)
-      speechSynthesis.speak(utterance);
-    } else {
-      console.error('Speech synthesis is not supported in this browser.');
-    }
-  }
-
-  function startRecognition() {
+function startRecognition() {
 
   if (!('webkitSpeechRecognition' in window)) {
     alert('Your browser does not support speech recognition.');
-    return;
-  }
-
-  if (recognition && listening) {
-    // Stop recognition if it's already running
-    recognition.stop();
-    listening = false;
-    console.log('Speech recognition stopped');
-    document.querySelector('.mic-button').classList.remove('blinking');
     return;
   }
 
@@ -36,14 +15,12 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
   }
 
   recognition = new SpeechRecognition();
-  recognition.continuous = true; 
+  recognition.continuous = false; // true doesn't work on Android devices.
   recognition.interimResults = false;
   recognition.lang = 'fr-FR';
 
   recognition.onstart = function() {
-    listening = true;
     console.log('Speech recognition started');
-    document.querySelector('.mic-button').classList.add('blinking');
   };
 
   let sentenceNumber = 0;
@@ -60,8 +37,10 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
   recognition.onend = function() {
     console.log('Speech recognition ended');
-    listening = false;
-    document.querySelector('.mic-button').classList.remove('blinking');
+    if (listening) {
+      console.log('Restarting speech recognition (until mic button is clicked)');
+      recognition.start();
+    }
   };
 
   recognition.start();
@@ -71,7 +50,19 @@ function addEventListeners() {
   const micButton = document.querySelector('.mic-button');
   if (micButton) {
     console.log('Adding event listener to mic button');
-    micButton.addEventListener('click', startRecognition);
+    micButton.addEventListener('click', function() {
+        console.log('Mic button clicked');
+        if (!listening) {
+            listening = true;
+            micButton.classList.add('blinking');
+            startRecognition();
+        }else{
+            listening = false;
+            micButton.classList.remove('blinking');
+            recognition.stop();
+        }
+      }
+    );
   }
 }
 
@@ -91,15 +82,11 @@ async function sendTextToServer(text) {
   
       const data = await response.json();
       console.log(data.message);
-      // console.log('Server response:', data.text);
-      // displayMessage('Server: ' + data.text);
       if (data.aiResponse) {
         if (data.reservationDetails) {
           updateBookingDetails(data.reservationDetails);
             displayMessage('AI: Réservation enregistrée. Merci et à bientôt!');
             speakText('Réservation enregistrée. Merci et à bientôt!');
-            // Toggle button to stop listening
-            startRecognition();
         } else {
           displayMessage('AI: ' + data.aiResponse);
             speakText(data.aiResponse);
@@ -108,7 +95,7 @@ async function sendTextToServer(text) {
     } catch (error) {
       console.error('Error:', error);
     }
-  }
+}
 
 function displayMessage(message) {
   const chatBox = document.getElementById('chat-box');
@@ -121,6 +108,18 @@ function displayMessage(message) {
   } else {
     console.error('Chat box element not found');
   }
+}
+
+function speakText(text) {
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = 'fr-FR'; // Set language to French
+      utterance.rate = 1.1; // Adjust speed (1 is normal)
+      utterance.pitch = 0.9; // Adjust pitch (1 is normal)
+      speechSynthesis.speak(utterance);
+    } else {
+      console.error('Speech synthesis is not supported in this browser.');
+    }
 }
 
 function updateBookingDetails(details) {
@@ -158,3 +157,4 @@ function updateBookingDetails(details) {
     userNameSpan.classList.add('filled');
   }
 }
+
